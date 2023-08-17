508506675552bb651f785e779bab7026b206078e
Wito <rudi_becker@outlook.com>
2023-08-17 09:21:46 -0300

    Corrigi algumas coisas no login, agora funcionando corretamente

--- src/main/java/escola/ti/controleparental/controller/LoginController.java
+++ src/main/java/escola/ti/controleparental/controller/LoginController.java
@@ -3,22 +3,22 @@ package escola.ti.controleparental.controller;
 import escola.ti.controleparental.model.UserModel;
 import escola.ti.controleparental.model.dto.EmailDTO;
 import escola.ti.controleparental.model.dto.LoginDTO;
-import escola.ti.controleparental.model.dto.SmsDTO;
 import escola.ti.controleparental.model.dto.UserLogin;
 import escola.ti.controleparental.model.util.EmailSenderService;
 import escola.ti.controleparental.model.util.Password;
 import escola.ti.controleparental.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
+
+//import escola.ti.controleparental.model.dto.SmsDTO;
+//import com.twilio.Twilio;
+//import com.twilio.rest.api.v2010.account.Message;
+//import com.twilio.type.PhoneNumber;
 
 @RestController
 @RequestMapping(path="/login")
@@ -65,13 +65,14 @@ public class LoginController {
     }
     */
 
-    @PostMapping(path="/login")
+    @PostMapping(path="/attempt")
     public String loginUsusario(@RequestBody LoginDTO body){
         for(UserModel u : userRepository.findAll()){
-            if(body.getLogin().equals(u.getEmail()) || body.getLogin().equals(u.getTelefone())){
+            if((body.getLogin().equals(u.getEmail()) || body.getLogin().equals(u.getTelefone())) && body.getSenha().equals(this.userLogin.getSenha())){
+                this.userLogin.setSenha(null);
                 return "Access Granted";
             }
         }
-        return "Access Failed";
+        return "Access Denied";
     }
 }
